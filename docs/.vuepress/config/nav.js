86f7b07d985463510368ffc143604ce38b9a5207
yubinCloud <yubin_SkyWalker@yeah.net>
2021-10-29 20:08:02 +0800

    完成DV算法

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,17 +1,30 @@
+
+/**
+ * 基础
+ */
+ basicItems = [
+  { text: '计算机网络', link: '/network/' },
+  { text: '组成与设计', link: '/organization/' },
+]
+
+/**
+ * AI
+ */
+aiItems = [
+  { text: '知识图谱', link: '/kg/' },
+]
+
 // nav
 module.exports = [
   { text: '首页', link: '/' },
   {
     text: '基础',
-    items: [
-      { text: '计算机网络', link: '/network/' },
-      { text: '组成与设计', link: '/organization/' },
-    ]
+    items: basicItems
   },
   {
     text: 'AI',
-    items: [
-      { text: '知识图谱', link: '/kg/' },
-    ]
+    items: aiItems
   },
 ]
+
+
