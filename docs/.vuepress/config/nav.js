537f7824aca8038014780f53bf20ddf3407ea39c
yubinCloud <yubin_SkyWalker@yeah.net>
2021-11-14 20:45:00 +0800

    #fix 修补内容bug

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,7 +4,7 @@
  */
  basicItems = [
   { text: '计算机网络', link: '/network/' },
-  { text: '组成与设计', link: '/computer-system/' },
+  { text: '计算机系统', link: '/computer-system/' },
   { text: '数据结构', link: '/data-structure/' },
 ]
 
