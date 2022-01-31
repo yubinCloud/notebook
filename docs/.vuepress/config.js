09cb51993420fd981719c8c1ed52731bd0aaac01
yubinCloud <yubin_SkyWalker@yeah.net>
2022-02-01 00:03:40 +0800

    #dev 将 html 的语言改为 zh-CN

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,17 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "notebook",
-  description: '学习笔记',
   base: '/notebook/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
+  },
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: "notebook",
+      description: "学习笔记"
+    }
   },
 
   head,
